1f2e1fce63f423f0ddb10bceb4538c90f996660c
SrinivasaPavanK <pavan.ispace@gmail.com>
2014-10-21 17:39:16 -0700

    Create EnableScanCount.java

---
+++ src/main/java/com/springweb/dynamodb/demo/repository/EnableScanCount.java
@@ -0,0 +1,17 @@
+package com.springweb.dynamodb.demo.repository;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Pavan KS
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Documented
+public @interface EnableScanCount {
+
+}
